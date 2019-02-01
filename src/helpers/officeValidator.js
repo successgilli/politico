b34f6db9b981dc16051753973b0fa6b294727819
successgilli <successgilli@gmail.com>
2019-02-01 12:52:46 +0100

    [finishes #-163649631] created post route for offices

---
+++ src/helpers/officeValidator.js
@@ -0,0 +1,29 @@
+class OfficeValidators {
+    // create party
+    static validateCreateOffice(req, res, next) {
+      const errorKeyMessage = 'missing a name key';
+      try {
+        const keys = Object.keys(req.body);
+        const lowerKeys = []; 
+        keys.forEach(x => lowerKeys.push(x.toLowerCase()) )
+       
+        if (!(lowerKeys.includes('id')) || 
+        !(lowerKeys.includes('type')) ||
+        !(lowerKeys.includes('name'))) {
+          throw new Error(errorKeyMessage);
+        } 
+       else {
+          next();
+        }
+      }
+      catch (err) {
+        const response = {
+          status: 400,
+          error: err.message
+        };
+        res.json(response);   
+      }
+    }
+  
+    } 
+  export default OfficeValidators;
