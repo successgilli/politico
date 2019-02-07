2c81c3bc2e1b0e7e8650f9fdb03e5886199c4ce7
successgilli <successgilli@gmail.com>
2019-02-07 02:20:32 +0100

    edited tests to reflect sql queries

--- src/controllers/partyController.js
+++ src/controllers/partyController.js
@@ -18,7 +18,7 @@ class PartyController {
     });  
   }
 
-// for get specific party route
+  // for get specific party route
   static getSpecificParty(req, res) {
     const text = 'SELECT * FROM parties WHERE id = $1';
     const param = [req.params.partyId];
@@ -26,55 +26,98 @@ class PartyController {
       if (err) {
         throw err;
       }
-      const response = {
-        status: 200,
-        data: [ {id: results.rows[0].id, name: results.rows[0].name, logoUrl: results.rows[0].logourl }]
+      else if (!results.rows[0]) {
+        const response = {
+          status: 400,
+          error: 'party not found',
+        };
+        res.status(200).json(response);
       }
-      res.status(200).json(response);
+      if (results.rows[0]) {
+        console.log(results)
+        const response = {
+          status: 200,
+          data: [ {id: results.rows[0].id, name: results.rows[0].name, logoUrl: results.rows[0].logourl }]
+        }
+        res.status(200).json(response);
+      }
     })
   }
-// get all parties
+  // get all parties
 
   static getAllParties(req, res) {
     const text = 'SELECT * FROM parties ';
-    pdb(text, (err, result)=>{
+    pdb(text, (err, result) => {
       if (err) {
-        throw err;
+        const response = {
+          status: 400,
+          error: err.message,
+        };
+        res.status(400).json(response);
+      } else {
+        const response = {
+          status: 200,
+          data: result.rows,
+        };
+        res.status(200).json(response);
       }
-      res.status(200).json(result.rows);
-    })
+    });
   }
 
-// edit party name
+  // edit party name
   static editPartyName(req, res) {
     const text = 'UPDATE parties SET name = $1 WHERE id = $2 RETURNING *';
     const param = [req.params.name, req.params.partyId];
-    pdb(text, param, (err, result)=>{
+    pdb(text, param, (err, result) => {
       if (err) {
-        throw err;
+        const response = {
+          status: 400,
+          error: err.message,
+        }
+        res.status(400).json(response);
       }
-      const response = {
-        status: 200,
-        data: [{id: result.rows[0].id, name: result.rows[0].name }]
+      else if (result.rows[0]) {
+        const response = {
+          status: 200,
+          data: [{ id: result.rows[0].id, name: result.rows[0].name }],
+        };
+        res.status(200).json(response);
       }
-      res.status(200).json(response);
-    })
+      else {
+        const response = {
+          status: 400,
+          error: 'id not found',
+        };
+        res.status(400).json(response);
+      }
+    });
   }
 
   // delete party
   static deletePartyName(req, res) {
-    const text = 'DELETE FROM parties WHERE id = $1';
+    const text = 'DELETE FROM parties WHERE id = $1 RETURNING *';
     const param = [req.params.partyId];
     pdb(text, param, (err, result) => {
       if (err) {
-        throw err;
+        const response = {
+          status: 400,
+          error: err.message,
+        };
+        res.status(400).json(response);
+      } else if (result.rows[0]) {
+        const response = {
+          status: 200,
+          data: [{ message: 'successfully deleted' }],
+        };
+        res.status(200).json(response);
+      } else {
+        const response = {
+          status: 400,
+          error: 'party not found',
+        };
+        res.status(400).json(response);
       }
-      const response = {
-        status: 200,
-        data: [ {message: 'successfully deleted'}]
-      }
-      res.status(200).json(response);
-    })
-}
+    });
+  }
 }
 export default PartyController;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../src/app';
+import pdb from '../src/model/query';
 
 let id;
 let name;
@@ -18,7 +19,7 @@ const officeData = {
 const badData = {
   hqAddress: 'somewhere',
   logoUrl: 'somethingLikeThat'
-}
+};
 chai.use(chaiHttp);
 //post create party
 describe('POST: parties', () => {
@@ -28,153 +29,127 @@ describe('POST: parties', () => {
       res.should.be.json;
       res.body.should.have.property('error').eql('missing a name key');
       done();
-    })   
-  })
-})
-  it('responed with the name and id of created party ', (done) => {
-    chai.request(server).post('/api/v1/parties').send(data).end((err, res) => { 
-      res.body.status.should.be.equal(200);
-      res.should.be.json;
-      res.body.data.should.be.a('array');
-      res.body.should.have.property('data');
-      res.body.data[0].should.have.property('name');
-      res.body.data[0].should.have.property('id');
-      id = res.body.data[0].id;      
-      res.body.data[0].name.should.be.a('string');
-      res.body.data[0].id.should.be.a('number');      
-      done();
-    })     
-  })
+    });   
+  });
+});
+it('responed with the name and id of created party ', (done) => {
+  chai.request(server).post('/api/v1/parties').send(data).end((err, res) => { 
+    res.body.status.should.be.equal(200);
+    res.should.be.json;
+    res.body.data.should.be.a('array');
+    res.body.should.have.property('data');
+    res.body.data[0].should.have.property('name');
+    res.body.data[0].should.have.property('id');
+    // eslint-disable-next-line prefer-destructuring
+    id = res.body.data[0].id;
+    res.body.data[0].name.should.be.a('string');
+    res.body.data[0].id.should.be.a('number');      
+    done();
+  });     
+});
   
 // get specific party //
 describe('GET: Specific party', () => {
   it('should respond with an error message when party not found', (done)=>{
-    chai.request(server).get('/api/v1/parties/kl').end((err, res) => {
+    chai.request(server).get('/api/v1/parties/100000').end((err, res) => {
       res.body.should.have.property('status').eql(400);
       res.should.be.json;
-      res.body.should.have.property('error').eql('Party not found');
+      res.body.should.have.property('error').eql('party not found');
       done();
-    })
-  })
-})
-  it('responed with the name, logo url and id of requested party', (done) => {
-    chai.request(server).get('/api/v1/parties/' + id).end((err, res) => {
-      res.body.status.should.be.equal(200);
-      res.should.be.json;
-      res.body.data.should.be.a('array');
-      res.body.should.have.property('data');
-      res.body.data[0].should.have.property('name');
-      res.body.data[0].should.have.property('id');
-      res.body.data[0].should.have.property('logoUrl');
-      res.body.data[0].name.should.be.a('string');
-      res.body.data[0].id.should.be.a('number');      
-      done();
-    })     
-  }) 
+    });
+  });
+});
+it('responed with the name, logo url and id of requested party', (done) => {
+  chai.request(server).get(`/api/v1/parties/${id}`).end((err, res) => {
+    res.body.status.should.be.equal(200);
+    res.should.be.json;
+    res.body.data.should.be.a('array');
+    res.body.should.have.property('data');
+    res.body.data[0].should.have.property('name');
+    res.body.data[0].should.have.property('id');
+    res.body.data[0].should.have.property('logoUrl');
+    res.body.data[0].name.should.be.a('string');
+    res.body.data[0].id.should.be.a('number');      
+    done();
+  });     
+}); 
 // get all parties
-for ( let i=0; i<5; i++){
-  describe('put data in db', () => {
-    it ('should input correct data. check for bad inputs', (done)=>{
-      chai.request(server).post('/api/v1/parties').send(badData).end((err, res) => {
-        res.body.should.have.property('status').eql(400);
-        res.should.be.json;
-        res.body.should.have.property('error').eql('missing a name key');
-        done();
-      })     
-    })
-    it('ensure posting works with the name and id of created party ', (done) => {
-      chai.request(server).post('/api/v1/parties').send(data).end((err, res) => { 
-        res.body.status.should.be.equal(200);
-        res.should.be.json;
-        res.body.data.should.be.a('array');
-        res.body.should.have.property('data');
-        res.body.data[0].should.have.property('name');
-        res.body.data[0].should.have.property('id');
-        id = res.body.data[0].id;       
-        res.body.data[0].name.should.be.a('string');
-        res.body.data[0].id.should.be.a('number');      
-        done();
-      })       
-    })
-    
-  })
-  // end
-}
 describe('GET: all party', () => {
-  it('responed with all parties in the database and corresponding information', (done) => {
+  it ('responed with all parties in the database and corresponding information', (done) => {
     chai.request(server).get('/api/v1/parties/').end((err, res) => {
       res.body.data.should.be.a('array') ; 
       res.should.be.json;
       res.body.data[0].should.be.a('object'); 
       res.body.status.should.equal(200);
-      res.body.data.length.should.equal(6); // based on the first post to db
+      pdb('SELECT * FROM parties ', (err, result) => {
+        const count = result.rowCount;
+        res.body.data.length.should.equal(count); // based on the information in the database
+      });
       done();
-    })     
-  })
-  
-})
+    });     
+  }); 
+});
 // PATCH edit party name
 describe('PATCH: edit party name', () => {
   it('should responed with an error message if id contains character', (done) => {
     chai.request(server).patch('/api/v1/parties/29769g8/bearsManyyy').end((err, res) => {
       res.body.should.have.property('error').eql('id invalid');
       done();
-    })     
-  })
+    });
+  });
   it('should responed with an error message if id is float', (done) => {
     chai.request(server).patch('/api/v1/parties/29769.8/bearsManyyy').end((err, res) => {
       res.body.should.have.property('error').eql('id should be integer number');
       done();
-    })     
-  })
+    });     
+  });
   it('should responed with an error message if name contains number', (done) => {
     chai.request(server).patch('/api/v1/parties/29769/bearsManyyy6').end((err, res) => {
       res.body.should.have.property('error').eql('name should be only string');
       done();
-    })     
-  })
+    });     
+  });
   it('should responed with an error message if id not found', (done) => {
     chai.request(server).patch('/api/v1/parties/429769/bearsManyyy').end((err, res) => {
-      res.body.should.have.property('error').eql('not found');
+      res.body.should.have.property('error').eql('id not found');
       done();
-    })     
-  })
+    });     
+  });
   it('should responed with the edited party', (done) => {
-    chai.request(server).patch('/api/v1/parties/' + id + '/bearsManyyy').end((err, res) => {
+    chai.request(server).patch(`/api/v1/parties/${id}/created`).end((err, res) => {
       res.body.should.have.property('data');
-      console.log(res.body.data)
-      res.body.data[0].name.should.equal("bearsManyyy");
+      res.body.data[0].name.should.equal("created");
       done();
-    })     
-  })
+    });     
+  });
   
   
-})
+});
 // DELETE party
 describe('DELETE: party', () => {
   it('should responed with an error message if id contains character', (done) => {
     chai.request(server).delete('/api/v1/parties/29769g8').end((err, res) => {
       res.body.should.have.property('error').eql('id invalid');
       done();
-    })     
-  })
+    });
+  });
   it('should responed with an error message if id is float', (done) => {
     chai.request(server).delete('/api/v1/parties/29769.8').end((err, res) => {
       res.body.should.have.property('error').eql('id should be integer number');
       done();
-    })     
-  })
+    });
+  });
   it('should responed with an error message if id not found', (done) => {
     chai.request(server).delete('/api/v1/parties/429769').end((err, res) => {
       res.body.should.have.property('error').eql('party not found');
       done();
-    })     
-  })
+    });
+  });
   it('should responed with the edited party', (done) => {
-    chai.request(server).delete('/api/v1/parties/' + id).end((err, res) => {
+    chai.request(server).delete(`/api/v1/parties/${id}`).end((err, res) => {
       res.body.should.have.property('data');
-      res.body.data[0].message.should.equal("successfully deleted");
+      res.body.data[0].message.should.equal('successfully deleted');
       done();
-    })     
-  })
-})
+    });
+  });
+});
